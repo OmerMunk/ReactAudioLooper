d58b17dbe2a06172884c29d8e673d9cdc07b9dda
Omer Munk <93129669+OmerMunk@users.noreply.github.com>
2022-04-19 17:26:48 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Enviroment from "../../react_loop_machine/src/Components/UI/Enviroment";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Welcome</h1>
+        <Enviroment/>
+    </>
   );
 }
 
---
+++ src/Utils.js
@@ -0,0 +1,11 @@
+export const musicFiles = [
+    'ALL TRACK.mp3',
+    'B VOC.mp3',
+    'DRUMS.mp3',
+    'HE HE VOC.mp3',
+    'HIGH VOC.mp3',
+    'JIBRISH.mp3',
+    'LEAD 1.mp3',
+    'UUHO VOC.mp3',
+    '_tambourine_shake_higher.mp3',
+]
